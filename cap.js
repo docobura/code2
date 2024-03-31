de199465531e600f5aba9028bcba4a908008649e
doc <docdaystar@gmail.com>
2024-03-31 23:13:10 +0300

    add dummy

--- cap.js
+++ cap.js
@@ -1,17 +1,17 @@
-const readline = require('readline');
+const readline = require('readline');//allows console manipulation
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+  input: process.stdin, //allows prnt in 
+  output: process.stdout //allows print out 
 });
 
 function swapCase(str) {
   let swappedStr = '';
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === str[i].toUpperCase()) {
-      swappedStr += str[i].toLowerCase();
+    if (str[i] === str[i].toUpperCase()) {// turns lower case into upper case
+      swappedStr += str[i].toLowerCase();// turns upper case to lower case
     } else {
-      swappedStr += str[i].toUpperCase();
+      swappedStr += str[i].toUpperCase();//swaps lower to upper and  uppeer to lower 
     }
   }
   return swappedStr;
@@ -19,6 +19,6 @@ function swapCase(str) {
 
 rl.question('Enter a string: ', (inputString) => {
   const swappedString = swapCase(inputString);
-  console.log('Swapped Case:', swappedString);
+  console.log('Swapped Case:', swappedString);//swaps the cases 
   rl.close();c
 });
